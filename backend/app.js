94f928d54d7449aa1259cd88c14038598123c745
Himanshu Singh <himanshusingh1k@gmail.com>
2026-06-16 10:34:46 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -21,18 +21,8 @@ connectDB();
 // API Routes
 app.use("/api", chatRoutes);
 
-/*------------------------------------ DEPLOYMENT ------------------------------------- */
-if (process.env.NODE_ENV === "production") {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => res.send("API is running..."));
-}
-/*------------------------------------ DEPLOYMENT ------------------------------------- */
+// Default Route
+app.get("/", (req, res) => res.send("API is running..."));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
